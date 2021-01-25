e0a133752a3b88e7f3d62e203a70dbaa79e06e31
adam stevenson <adamscdcm@gmail.com>
2021-01-24 22:11:03 -0800

    added rows col, initial JS

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+
+//display current day 
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+console.log(moment) 
+//moment().seconds(30).valueOf() === new Date().setSeconds(30);
+//moment().seconds()   === new Date().getSeconds();
